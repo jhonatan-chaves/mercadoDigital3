3fe9e61e21201af3c3bdf5cb7e5d0c0dd8f27af3
jhonatan-chaves <cjonata799@gmail.com>
2022-08-09 14:06:48 -0300

    agora sim

--- src/br/com/mercadoDigital3/AppMercado3.java
+++ src/br/com/mercadoDigital3/AppMercado3.java
@@ -3,6 +3,7 @@ package br.com.mercadoDigital3;
 public class AppMercado3 {
 
 	public static void main(String[] args) {
+		System.out.println("thaisa");
 		System.out.println("jhonatan");
 		// TODO Auto-generated method stub
 
