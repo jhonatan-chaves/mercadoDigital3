49866b65d3b88a079020a4301adbb0721c928106
jhonatan-chaves <cjonata799@gmail.com>
2022-08-10 13:38:25 -0300

    testando metodos com e sem retorno

--- src/br/com/mercadoDigital3/AppMercado3.java
+++ src/br/com/mercadoDigital3/AppMercado3.java
@@ -9,7 +9,7 @@ public class AppMercado3 {
 		
 		
 	    String nome = JOptionPane.showInputDialog( "qual o seu nome? ");
-	    String idade = JOptionPane.showInputDialog("qual a sua idade? ");
+	  /*  String idade = JOptionPane.showInputDialog("qual a sua idade? ");
 	    Integer number = Integer.valueOf(idade);
 	    String email = JOptionPane.showInputDialog("qual o seu E-mail? ");	 
 	    String cpf = JOptionPane.showInputDialog("qual o seu CPF? ");
@@ -17,22 +17,31 @@ public class AppMercado3 {
 	    String nomeMae = JOptionPane.showInputDialog("qual o nome da sua mãe? ");
 	    String nomePai = JOptionPane.showInputDialog("qual o nome do seu pai? ");
 	    String numeroCel = JOptionPane.showInputDialog("qual o seu numero de celular? ");
+	   */ 
+	    
+	    
+	    
 	    
 		
 		aluno1.setNome(nome);
-		aluno1.setIdade(number);
+		/*aluno1.setIdade(number);
 		aluno1.setEmail(email);
 		aluno1.setCpf(cpf);
 		aluno1.setDataNascimento(dataNascimento);
 		aluno1.setNomeMae(nomeMae);
 		aluno1.setNomePai(nomePai);
 		aluno1.setNumeroCel(numeroCel);
+		*/
+		aluno1.setNota1(60);
+		aluno1.setNota2(90);
+		aluno1.setNota3(80);
+		aluno1.setNota4(88);
 		
-		
-		
+		System.out.println(aluno1.mediaAluno());
 		
 		
 		System.out.println(aluno1.toString());
+		System.out.println((aluno1.alunoAprovado() ? "aprovado" : "reprovado"));
 		
 		
 
